314c777adb90e400082875bad0eb6e1ce75af49e
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2026-02-28 13:37:56 -0700

    Use variable project name in build script

--- dev-scripts/make-msixbundle.ps1
+++ dev-scripts/make-msixbundle.ps1
@@ -3,8 +3,8 @@ $ProjectFile = "ZedCommandPalette/ZedCommandPalette.csproj"
 $xml = [xml](Get-Content $ProjectFile)
 $Version = $xml.Project.PropertyGroup.AppxPackageVersion | Select-Object -First 1
 
-dotnet build ZedCommandPalette/ZedCommandPalette.csproj --configuration Release -p:GenerateAppxPackageOnBuild=true -p:Platform=ARM64 -p:SelfContained=true -p:RuntimeIdentifier=win-arm64 -p:AppxPackageDir="../AppPackages/ARM64/"
-dotnet build ZedCommandPalette/ZedCommandPalette.csproj --configuration Release -p:GenerateAppxPackageOnBuild=true -p:Platform=x64 -p:SelfContained=true -p:RuntimeIdentifier=win-x64 -p:AppxPackageDir="../AppPackages/x64/"
+dotnet build $ProjectFile --configuration Release -p:GenerateAppxPackageOnBuild=true -p:Platform=ARM64 -p:SelfContained=true -p:RuntimeIdentifier=win-arm64 -p:AppxPackageDir="../AppPackages/ARM64/"
+dotnet build $ProjectFile --configuration Release -p:GenerateAppxPackageOnBuild=true -p:Platform=x64 -p:SelfContained=true -p:RuntimeIdentifier=win-x64 -p:AppxPackageDir="../AppPackages/x64/"
 
 $x64Msix = Get-ChildItem -Path AppPackages/x64 -Recurse -Filter *.msix | Select-Object -First 1
 $arm64Msix = Get-ChildItem -Path AppPackages/ARM64 -Recurse -Filter *.msix | Select-Object -First 1
